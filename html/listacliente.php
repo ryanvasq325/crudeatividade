b4357e63a0f97a1a8e2fde75fe5a25f5a41f52b1
ryanvasq325 <ryanvasques77@gmail.com>
2025-10-21 16:04:12 -0400

    altercao
    alteracao

--- html/listacliente.php
+++ html/listacliente.php
@@ -13,120 +13,143 @@
     <form id="form">
         <input type="hidden" id="id_cliente" name="id_cliente">
         <input type="hidden" id="acao" name="acao">
+
+        <!-- Navbar -->
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navegação</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="/listausuario.php">Lista de Usuarios</a>
+                        <a class="nav-link active" aria-current="page" href="/listausuario.php">Lista de Usuários</a>
                     </div>
                 </div>
             </div>
         </nav>
 
-        <div class="modal fade" id="excluirRegistroCliente" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="excluirRegistroClienteLabel" aria-hidden="true">
+        <!-- Modal de exclusão -->
+        <div class="modal fade" id="excluirRegistroCliente" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="excluirRegistroClienteLabel" aria-hidden="true">
             <div class="modal-dialog modal-sm">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="excluirRegistroClienteLabel">
-                            Atenção
-                        </h1>
+                        <h1 class="modal-title fs-5" id="excluirRegistroClienteLabel">Atenção</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Deseja realmente excluir o registro
+                        Deseja realmente excluir o registro?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">
-                            <i class="fa-solid fa-ban"></i>
-                            Cancelar
+                            <i class="fa-solid fa-ban"></i> Cancelar
                         </button>
                         <button type="button" class="btn btn-outline-danger" id="excluirRegistro">
-                            <i class="fa-solid fa-trash"></i>
-                            Excluir
+                            <i class="fa-solid fa-trash"></i> Excluir
                         </button>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
-        <div class="modal fade" id="cadastroCliente" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="cadastroClienteLabel" aria-hidden="true">
+
+        <!-- Modal de cadastro -->
+        <div class="modal fade" id="cadastroCliente" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="cadastroClienteLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="cadastroClienteLabel">Cadastro de cliente</h1>
+                        <h1 class="modal-title fs-5" id="cadastroClienteLabel">Cadastro de Cliente</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="row">
+                            <!-- Nome -->
                             <div class="col-6">
                                 <div class="form-floating mb-3 mt-3">
-                                    <input type="search" class="form-control" id="nome_completo" name="nome_completo" placeholder="pesquisa">
-                                    <label for="nome_completo">Digite seu nome completo</label>
+                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required>
+                                    <label for="nome">Nome</label>
                                 </div>
                             </div>
+                            <!-- Sobrenome -->
                             <div class="col-6">
                                 <div class="form-floating mb-3 mt-3">
-                                    <input type="search" class="form-control" id="cpf" name="cpf" placeholder="pesquisa">
-                                    <label for="cpf">Digite seu CPF</label>
+                                    <input type="text" class="form-control" id="sobrenome" name="sobrenome" placeholder="Digite seu sobrenome" required>
+                                    <label for="sobrenome">Sobrenome</label>
+                                </div>
+                            </div>
+                            <!-- CPF -->
+                            <div class="col-6">
+                                <div class="form-floating mb-3 mt-3">
+                                    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite seu CPF" required>
+                                    <label for="cpf">CPF</label>
+                                </div>
+                            </div>
+                            <!-- RG -->
+                            <div class="col-6">
+                                <div class="form-floating mb-3 mt-3">
+                                    <input type="text" class="form-control" id="rg" name="rg" placeholder="Digite seu RG" required>
+                                    <label for="rg">RG</label>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">
-                            <i class="fa-solid fa-ban"></i>
-                            Cancelar
+                            <i class="fa-solid fa-ban"></i> Cancelar
                         </button>
                         <button type="button" class="btn btn-outline-success" id="salvarRegistro">
-                            <i class="fa-solid fa-floppy-disk"></i>
-                            Salvar
+                            <i class="fa-solid fa-floppy-disk"></i> Salvar
                         </button>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container">
+
+        <!-- Container principal -->
+        <div class="container mt-3">
             <div class="row">
-                <div class="col-12 mt-2">
-                    <button type="button" class="btn btn-outline-success" title="Clique para cadastrar um novo cliente" data-bs-toggle="modal" data-bs-target="#cadastroCliente">
-                        <i class="fa-solid fa-plus"></i>
-                        Cadastro
+                <!-- Botão de cadastro -->
+                <div class="col-12 mb-3">
+                    <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#cadastroCliente">
+                        <i class="fa-solid fa-plus"></i> Cadastro
                     </button>
                 </div>
-                <div class="col-12">
-                    <div class="col-12">
-                        <div class="form-floating mb-3 mt-3">
-                            <input type="search" class="form-control" id="pesquisa" name="pesquisa" placeholder="pesquisa">
-                            <label for="pesquisa">Digite sua pesquisa</label>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <table class="table table-strip table-hover table-bordered mt-3">
-                            <thead>
-                                <tr>
-                                    <th>Código</th>
-                                    <th>Nome</th>
-                                    <th>Nome</th>
-                                    <th>CPF</th>
-                                    <th>RG</th>
-                                    <th>Ativo</th>
-                                    <th>Ação</th>
-                                </tr>
-                            </thead>
-                            <tbody id="listaCliente"></tbody>
-                        </table>
+
+                <!-- Campo de pesquisa -->
+                <div class="col-12 mb-3">
+                    <div class="form-floating">
+                        <input type="search" class="form-control" id="pesquisa" name="pesquisa" placeholder="Digite sua pesquisa">
+                        <label for="pesquisa">Pesquisar Cliente</label>
                     </div>
                 </div>
+
+                <!-- Tabela de clientes -->
+                <div class="col-12">
+                    <table class="table table-striped table-hover table-bordered mt-3">
+                        <thead>
+                            <tr>
+                                <th>Código</th>
+                                <th>Nome</th>
+                                <th>Sobrenome</th>
+                                <th>CPF</th>
+                                <th>RG</th>
+                                <th>Ativo</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody id="listaCliente"></tbody>
+                    </table>
+                </div>
             </div>
+        </div>
     </form>
+
+    <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="/js/bootstrap.js"></script>
     <script src="/js/all.js"></script>
     <script src="/js/listacliente.js"></script>
 </body>
-
 </html>
